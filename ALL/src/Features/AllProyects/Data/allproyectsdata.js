bcd0960d362814a923101433b666fa7fe80ab78c
Below-Z3R0 <e.centenopena@gmail.com>
2026-03-13 04:08:51 +0100

    Test

--- ALL/src/Features/AllProyects/Data/allproyectsdata.js
+++ ALL/src/Features/AllProyects/Data/allproyectsdata.js
@@ -1,50 +1,49 @@
-import img from "../../../assets/Img/5.png"
-import img2 from "../../../assets/Img/2.jpg"
+import CSS3V2 from "../../..//assets/Img/CSS3V2.svg"
 export const AllproyectsData = {
     General: {
     Gene: [
         {id: 1, 
-        ImgSrc: img2, 
+        ImgSrc: CSS3V2, 
         Title: "Primero",
         Description: "Descripcion original",
         ImgsContainer: [
-        {Logoid: 1, Logo: img, Description: "1"},
-        {Logoid: 2, Logo: img, Description: "2"},
-        {Logoid: 3, Logo: img, Description: "3"},
-        {Logoid: 4, Logo: img, Description: "4"}
+        {Logoid: 1, Logo: CSS3V2, Description: "1"},
+        {Logoid: 2, Logo: CSS3V2, Description: "2"},
+        {Logoid: 3, Logo: CSS3V2, Description: "3"},
+        {Logoid: 4, Logo: CSS3V2, Description: "4"}
     ],},
 
         {id: 2, 
-        ImgSrc: img2, 
+        ImgSrc: CSS3V2, 
         Title: "Segundo",
         Description: "Descripcion original",
         ImgsContainer: [
-        {Logoid: 1, Logo: img, Description: "1"},
-        {Logoid: 2, Logo: img, Description: "2"},
-        {Logoid: 3, Logo: img, Description: "3"},
-        {Logoid: 4, Logo: img, Description: "4"}
+        {Logoid: 1, Logo: CSS3V2, Description: "1"},
+        {Logoid: 2, Logo: CSS3V2, Description: "2"},
+        {Logoid: 3, Logo: CSS3V2, Description: "3"},
+        {Logoid: 4, Logo: CSS3V2, Description: "4"}
     ],},
 
         {id: 3, 
-        ImgSrc: img2, 
+        ImgSrc: CSS3V2, 
         Title: "Tercero",
         Description: "Descripcion original",
         ImgsContainer: [
-        {Logoid: 1, Logo: img, Description: "1"},
-        {Logoid: 2, Logo: img, Description: "2"},
-        {Logoid: 3, Logo: img, Description: "3"},
-        {Logoid: 4, Logo: img, Description: "4"}
+        {Logoid: 1, Logo: CSS3V2, Description: "1"},
+        {Logoid: 2, Logo: CSS3V2, Description: "2"},
+        {Logoid: 3, Logo: CSS3V2, Description: "3"},
+        {Logoid: 4, Logo: CSS3V2, Description: "4"}
     ],},
 
         {id: 4, 
-        ImgSrc: img2, 
+        ImgSrc: CSS3V2, 
         Title: "Cuarto",
         Description: "Descripcion original",
         ImgsContainer: [
-        {Logoid: 1, Logo: img, Description: "1"},
-        {Logoid: 2, Logo: img, Description: "2"},
-        {Logoid: 3, Logo: img, Description: "3"},
-        {Logoid: 4, Logo: img, Description: "4"}
+        {Logoid: 1, Logo: CSS3V2, Description: "1"},
+        {Logoid: 2, Logo: CSS3V2, Description: "2"},
+        {Logoid: 3, Logo: CSS3V2, Description: "3"},
+        {Logoid: 4, Logo: CSS3V2, Description: "4"}
     ],},          
     ]}
 }
